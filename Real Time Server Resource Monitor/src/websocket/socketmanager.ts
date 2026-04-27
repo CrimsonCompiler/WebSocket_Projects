f3ae461027a776d43ffeb91e2f41d111bf509398
CrimsonCompiler <toisiftasrik@gmail.com>
2026-04-27 12:25:35 +0600

    fix: correct WebSocket connection handling and improve client disconnection logging

--- Real Time Server Resource Monitor/src/websocket/socketmanager.ts
+++ Real Time Server Resource Monitor/src/websocket/socketmanager.ts
@@ -1,13 +1,26 @@
 import http from "http";
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
+import { getSystemMetrics } from "../services/metricsService";
 
 export const setupWebSocket = (server: http.Server) => {
   const wss = new WebSocketServer({ server });
 
-  wss.on("connection", (ws: WebSocketServer) => {
+  wss.on("connection", (ws: WebSocket) => {
     console.log("New Client connected!");
-    wss.on("close", () => {
-      console.log("Client disconnected..");
+
+    ws.on("close", () => {
+      console.log("Client disconnected.");
     });
   });
+
+  // BroadCasting loop
+  setInterval(() => {
+    getSystemMetrics((metrics) => {
+      wss.clients.forEach((client: WebSocket) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(metrics));
+        }
+      });
+    });
+  }, 1000);
 };
