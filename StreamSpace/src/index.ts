e503ab37f101784bf3cbc413746de8b2a33ff229
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-01 20:35:31 +0600

    feat: refactor server setup to use http server for WebSocket integration

--- StreamSpace/src/index.ts
+++ StreamSpace/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRouter from "./routes/index.js";
+import { setupWebSocket } from "./websocket/socketManager.js";
+import http from "http";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,14 +13,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// http server
+const server = http.createServer(app);    
+
 // Routes
 app.use("/api/v1", authRouter);
+
+// Websocket
+setupWebSocket(server);
 
 // Health check
 app.get("/", (req, res) => {
   res.json({ status: "ok", message: "StreamSpace API is running" });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
